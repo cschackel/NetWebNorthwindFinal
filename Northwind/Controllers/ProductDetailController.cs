95949d9031de3f2f1ed410d3c869404623217a3d
smarshall5 <smarshall5@my.wctc.edu>
2019-11-19 13:49:04 -0600

    Stuff

---
+++ Northwind/Controllers/ProductDetailController.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Northwind.Models;
+
+namespace Northwind.Controllers
+{
+    public class ProductDetailController : Controller
+    {
+        private readonly INWRepo _db;
+
+        public ProductDetailController(INWRepo db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index(int id) => View(_db.Products.Include(p => p.Category)
+            .Where(p => p.Discontinued == false).First(p => p.ProductId == id));
+    }
+}
